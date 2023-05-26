535851cc3b48d8363ea827d3d876c033f9e52fb1
tsharmilareddy <tsharmilareddy47@gmail.com>
2023-05-26 08:24:47 +0530

    boto3 added

---
+++ rebooted ec2instance.py
@@ -0,0 +1,21 @@
+
+
+
+# import boto3
+
+# AWS_REGION = "eu-west-3"
+# EC2_RESOURCE = boto3.resource('ec2', region_name=AWS_REGION)
+# INSTANCE_ID = 'i-0cb8fc27bd83b1889'
+
+# instance = EC2_RESOURCE.Instance("i-0cb8fc27bd83b1889")
+
+# instance.reboot()
+
+# print('EC2 instance "{"i-0cb8fc27bd83b1889"}" has been rebooted')
+
+
+
+
+
+age =input("My age is: " )
+print (age)
